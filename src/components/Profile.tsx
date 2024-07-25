098cb7497b918085f928a6be582e29d8c5e56906
Fabrice-Perso <magnan.fabrice@gmail.com>
2024-07-26 01:54:35 +0200

    correction2

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
 import profilePic from '../assets/profil.jpg';
+
 const Profile: React.FC = () => {
   return (
     <div className="mb-8">
+      <div className="w-32 h-32 mx-auto mb-4 relative">
         <Image 
           src={profilePic} 
           alt="Photo" 
-          className="rounded-full mx-auto mb-4"
-          width={128} 
-          height={128} 
+          className="rounded-full"
+          layout="fill"
+          objectFit="cover"
         />
+      </div>
       <h2 className="text-lg font-bold mb-2">RÉSUMÉ</h2>
       <p className="mb-2">
-        Développeur web spécialisé en React et PHP avec 3 ans d'expérience, je suis issu d'une
+        {`Développeur web spécialisé en React et PHP avec 3 ans d'expérience, je suis issu d'une
         solide expérience en informatique qui a orienté ma reconversion vers ma passion pour le
-        développement web.
+        développement web.`}
       </p>
       <p className="mb-2">
         Agile dans la résolution de problèmes et créatif.
       </p>
       <p>
-        Mon parcours est marqué par une évolution constante et une quête d'excellence
-        technologique.
+        {`Mon parcours est marqué par une évolution constante et une quête d'excellence
+        technologique.`}
       </p>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,26 +7,26 @@ const Projects: React.FC = () => {
       <h3 className="font-semibold">Gestion des Comptes Personnels (Format Web)</h3>
       <p className="text-sm">07/2023 - Présent</p>
       <p className="mb-4">
-        En prévision de ma formation et pour pratiquer, j'ai choisi de transformer la version Excel
+        {`En prévision de ma formation et pour pratiquer, j'ai choisi de transformer la version Excel
         VBA en format Web. Cela permettra une plus grande flexibilité, de le rendre plus esthétique
-        et d'augmenter la capacité des utilisateurs.
+        et d'augmenter la capacité des utilisateurs.`}
       </p>
       <ul className="list-disc list-inside mb-4">
         <li>Utilisation de React et Redux pour le frontend</li>
-        <li>Création d'une API Sécurisée avec le Framework Laravel pour PHP</li>
+        <li>{`Création d'une API Sécurisée avec le Framework Laravel pour PHP`}</li>
         <li>Hébergement du projet sur mon NAS Synology</li>
         <li>Création de mon propre serveur de mail sur mon NAS</li>
       </ul>
       <h3 className="font-semibold">Gestion des Comptes Personnels (VBA Excel)</h3>
       <p className="text-sm">01/2019 - Présent</p>
       <p className="mb-4">
-        Création d'un outil sous Excel VBA pour la gestion quotidienne de mes comptes bancaires, lié
+        {`Création d'un outil sous Excel VBA pour la gestion quotidienne de mes comptes bancaires, lié
         à une base de données. Possibilité de l'utiliser à plusieurs et de gérer des comptes communs,
-        avec sauvegarde automatique.
+        avec sauvegarde automatique.`}
       </p>
       <ul className="list-disc list-inside">
         <li>Utilisation de VBA sous Excel</li>
-        <li>Utilisation d'une base de données sous Access</li>
+        <li>{`Utilisation d'une base de données sous Access`}</li>
       </ul>
     </div>
   );
