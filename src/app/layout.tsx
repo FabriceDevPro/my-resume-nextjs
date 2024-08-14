aad4bdd31a25c2ab96b4a1004e6266e05d20bdfb
Fabrice-Perso <magnan.fabrice@gmail.com>
2024-08-14 19:05:18 +0200

    correction meta + analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,38 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: "CV Fabrice Magnan de Bellevue",
   description: "Découvrez le CV complet de Fabrice Magnan de Bellevue, un développeur web full stack spécialisé en HTML, CSS, SASS, JavaScript, React, Vite.js, PHP, et PHPMyAdmin.",
-  icons: {
-    icon: "/favicon.png",
-  },
+  authors: [{ name: "Fabrice Magnan" }],
+  keywords: [
+    "freelance développeur web",
+    "création de site internet",
+    "développement web sur mesure",
+    "SEO",
+    "développeur full stack",
+    "FabWebProjects",
+    "Occitanie",
+    "France",
+    "indépendant",
+    "PME",
+    "React Developer",
+    "Full-Stack Developer",
+    "Vite.js Specialist",
+    "JavaScript Expert",
+    "Remote Developer France"
+  ],
   openGraph: {
-    title: "CV Fabrice Magnan de Bellevue",
+    title: "Développeur Web Full-Stack - CV Fabrice Magnan de Bellevue",
     description: "Le CV en ligne de Fabrice Magnan de Bellevue, développeur web full stack avec une expertise en HTML, CSS, SASS, JavaScript, React, Vite.js, PHP, et PHPMyAdmin.",
     url: "https://my-resume.fabwebprojects.fr/", 
     siteName: "CV de Fabrice Magnan de Bellevue",
     type: "profile",
+    images: [
+      {
+        url: "https://my-resume.fabwebprojects.fr/description.png",
+        width: 1200,
+        height: 630,
+        alt: "Développeur Web Freelance - FabWebProjects | Fabrice Magnan",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
